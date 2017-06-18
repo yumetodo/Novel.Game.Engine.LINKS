320c1c095a8feafe4ea9e91eeda8b3100655b7d7
yumetodo <yume-wikijp@live.jp>
2017-06-18 17:55:41 +0900

    fix(keystate): change file name

--- keystate.cpp
+++ keystate.cpp
@@ -1,4 +1,4 @@
-#include "Dxkeystate.h"
+﻿#include "keystate.hpp"
 #	if !defined(CINTERFACE) && defined(__c2__) &&  __clang_major__ == 3 && __clang_minor__ == 8
 //To avoid compile error
 //C:\Program Files (x86)\Windows Kits\8.1\Include\um\combaseapi.h(229,21): error : unknown type name 'IUnknown'
